d6b01e8dbe7d468dd2a6734c885a159fbf8dc5cc
MurilloGodoi <murillogodoi13@hotmail.com>
2021-01-26 15:38:34 -0300

    Some changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
@@ -6,6 +6,7 @@ import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import Head from 'next/head'
+import { useRouter } from 'next/router';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -26,21 +27,37 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
-   
+
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>SPFC QUIZ</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="SPFC Quiz" key="title" />
+        <meta property="og:image" content="https://wallpaperaccess.com/full/1949839.jpg" />
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <title>SPFC QUIZ</title>
             <h1>Quiz do São Paulo FC</h1>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            <meta property="og:title" content="SPFC Quiz" key="title" />
-            <meta property="og:image" content="https://wallpaperaccess.com/full/1949839.jpg"/>
           </Widget.Header>
           <Widget.Content>
-            <p>Entre no Quiz!</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              
+              router.push(`/quiz?name=${name}`);
+              console.log("Fazendo uma submissão por meio do formulario");
+            }
+
+            }>
+              <input onChange={function (infos) {
+                setName(infos.target.value);
+              }}  placeholder="Digite seu nome" />
+              <button type="submit" disabled={name.length === 0}>JOGAR</button>
+            </form>
 
           </Widget.Content>
         </Widget>
